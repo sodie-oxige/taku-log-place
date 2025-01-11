e353034fcdf661a0dd10f03943bbd0496c8457fd
sodie-oxige <sodie.oxige@gmail.com>
2025-01-11 15:09:44 +0900

    electronで動くように変更

---
+++ main.ts
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
